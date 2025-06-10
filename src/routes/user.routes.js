2b67acbc1ea8d64b7f4a6a9e6c656a1341c1f0d0
Lomesh Gupta <guptabittu06@gmail.com>
2025-06-10 23:45:46 +0530

    sync

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,4 @@ const express = require("express");
 const router = express.Router();
 const { getAllUsers, createUser } = require("../controllers/user.controller");
 
-router.get("/", getAllUsers);
-router.post("/", createUser);
-
 module.exports = router;
